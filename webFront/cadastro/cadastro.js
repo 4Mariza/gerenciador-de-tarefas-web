0fc582c4be9221b356a869cdb7e841776ac8b198
VITOR SANTANA DE JESUS <sn1079379@cfp127aluno.local>
2024-02-06 18:25:29 -0300

    ajustes

--- webFront/cadastro/cadastro.js
+++ webFront/cadastro/cadastro.js
@@ -1,51 +1,45 @@
 const button = document.getElementById('button_cadastrar')
-// function cadastrarUsuario(usuario){
 
-//     const url = "http://localhost:8080/usuario"
+async function cadastrarUsuario(usuario){
 
-//     if(email === '' || senha === '' || nome === ''){
-//         alert('Preencha os campos corretamente!')
-//     }
+    const url = "http://localhost:3000/usuario"
 
-//     console.log(usuario);
-
-//     try{
-//         const options = {
-//             method: "POST",
-//             headers: {
-//               "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify(usuario),
-//           }
-
-
-
-
-//         fetch(url, options)
-
-//         }catch (error){
-//         console.error(error)
-//     }
-// } 
-
-button.addEventListener("click", () => {
-    const user = {
-        nome: document.getElementById("nome").value,
-        email: document.getElementById("email").value,
-        senha: document.getElementById("password").value
+    if(email === '' || senha === '' || nome === ''){
+        alert('Preencha os campos corretamente!')
     }
 
-    console.log(nome)
+    try{
+        const options = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(usuario),
+          }
 
-    fetch('http://localhost:8080/usuario', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-    })
+        await fetch(url, options)
 
-    // window.location.href = '../inicial/inicial.html'
+    } catch (error){
+        console.error(error)
+    }
+} 
+
+button.addEventListener("click", () => {
+
+    const nome = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("password").value;
+
+    const user = {
+        nome: nome,
+        email: email,
+        senha: senha
+    }
+
+    cadastrarUsuario(user)
+
+
+    window.location.href = '../inicial/inicial.html'
     alert('Usuário cadastrado com sucesso!')
 })
 
