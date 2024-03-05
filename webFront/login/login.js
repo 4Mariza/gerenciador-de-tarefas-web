71abd89ef970ea16b2cd238a6505c343113412e6
MARIA LUIZA GOMES DE ALMEIDA <23111819@CFP127ALUNO.LOCAL>
2024-03-05 17:27:50 -0300

    finalizar comentarios...

--- webFront/login/login.js
+++ webFront/login/login.js
@@ -13,11 +13,23 @@ async function validarLogin(){
         listUsers.forEach((user) => {
             if(email === user.email && senha === user.senha){
 
+                Toastify({
+                    text: "Usuário Logado com Sucesso!",
+                    duration: 5000,
+                    style: {
+                        background: "linear-gradient(to right, #00b09b, #96c93d)",
+                        width: '200px',
+                        height: '30px',
+                        textAlign: 'center',
+                        alignItems: 'center',
+                    }
+                }).showToast();
+
                 localStorage.setItem('user',user.nome)
                 localStorage.setItem('id', user.id)
                 localStorage.setItem('email', user.email)
 
-                alert('User logado com sucesso!!')
+                // alert('User logado com sucesso!!')
                 window.location.href = "../inicial/inicial.html"
             }
         })
